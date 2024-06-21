8917eba7df1b01de701985356ef1b6f3a6debeb5
alex.wambugu@student.moringaschool.com <wambugumaigua@DESKTOP-F32N3MJ>
2024-06-21 10:15:46 -0700

    complete

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,24 @@
 class Song:
-    pass
+    count = 0
+    genre_count = {}
+    artist_count = {}
+    genres = set()
+    artists = set()
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.count += 1
+        if genre not in Song.genre_count:
+            Song.genre_count[genre] = 1
+        else:
+            Song.genre_count[genre] += 1
+        if artist not in Song.artist_count:
+            Song.artist_count[artist] = 1
+        else:
+            Song.artist_count[artist] += 1
+        if genre not in Song.genres:
+            Song.genres.add(genre)
+        if artist not in Song.artists:
+            Song.artists.add(artist)
